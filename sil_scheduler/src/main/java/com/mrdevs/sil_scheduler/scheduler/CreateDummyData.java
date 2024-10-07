6aecbd89390cc2e2d3833875bf502666f55d407d
M Ridho <ridhomuhammad.rb@gmail.com>
2024-10-07 18:49:07 +0700

    refactor: adding comments

--- sil_scheduler/src/main/java/com/mrdevs/sil_scheduler/scheduler/CreateDummyData.java
+++ sil_scheduler/src/main/java/com/mrdevs/sil_scheduler/scheduler/CreateDummyData.java
@@ -21,26 +21,35 @@ public class CreateDummyData {
     @Autowired
     private KafkaProducer kafkaProducer;
 
+    // topic name for locomotive data
     private String topics = "locomotive-data-test-topic";
-    private Integer[] status = { 1, 2, 3 };
+    // static data for randomize data
+    private Integer[] status = { 1, 2, 3 }; // 1: On Duty, 2: On Depot, 3: Under Maintenance
     private String[] type = { "Executive", "Bussiness", "Economy" };
 
     @Scheduled(fixedDelay = 10000)
     public void scheduler() throws JsonProcessingException {
         Random randomize = new Random();
 
+        // create random code
         String code = "LOCO-" + UUID.randomUUID().toString().substring(0, 3).toUpperCase();
+        // create random name based on code + type list
         String name = code + " " + type[randomize.nextInt(type.length)];
 
+        // generate dummy data
         Locomotive locomotive = Locomotive.builder().code(code).name(name)
                 .dimension("2000 x 15000").statusId(status[randomize.nextInt(status.length)]).build();
 
         ObjectMapper objectMapper = new ObjectMapper();
+        // convert object class to json string
         String message = objectMapper.writeValueAsString(locomotive);
 
+        // sending dummy data to kafka which later will be stored to postgresql and to
+        // consume for another service
         kafkaProducer.sendMessage(topics, message);
 
         // System.out.println("Sending message : " + message);
+        // logging event
         logger.info("Dummy data has been created : " + message);
     }
 
